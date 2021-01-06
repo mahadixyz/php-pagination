e544ad78858e5c9ca82e5c6a223a6919d4b37a4b
Mahadi Hasan <mahadihk@outlook.com>
2021-01-06 17:22:41 +0600

    Minor change on UI

--- index.php
+++ index.php
@@ -23,14 +23,14 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Data with pagination</title>
 </head>
 
 <body>
 
     <div class="container overflow-hidden">
         <div class="row g-3">
-            <div class="col-8 mx-auto my-3">
+            <div class="col-8 mt-5 mx-auto">
                 <div class="border p-3">
                 <?php    
                     if(isset($_SESSION['err']))
@@ -113,7 +113,7 @@
         </div>
     </div>
 
-
+    <!-- Bootstrap js CDN -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 
